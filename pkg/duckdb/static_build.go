e21d7f624b522e6c39c8d7887a49fbbf5d2ed1cb
Sven Herrmann <sven.herrmann@thatscalaguy.de>
2026-03-25 07:02:36 +0100

    refactor: remove static DuckDB build support and related configurations

--- pkg/duckdb/static_build.go
+++
@@ -1,8 +0,0 @@
-//go:build duckdb_use_static_lib
-
-package duckdb
-
-// staticBuild is true when linking against a custom libduckdb_bundle.a
-// that has extensions compiled in. Bundled extensions only need LOAD
-// (not INSTALL) since they are part of the static library.
-const staticBuild = true
